6e7f7e37c2b0939a81d4d12cbb6e704f6d5039ea
LAPTOP-92NBAG1S\lenovo <nomai6789@gmail.com>
2023-04-24 15:31:54 +0700

    ds

--- src/app_QLMBX/Frm_DanhDachHopDong.java
+++ src/app_QLMBX/Frm_DanhDachHopDong.java
@@ -1,5 +1,48 @@
 package app_QLMBX;
 
-public class Frm_DanhDachHopDong {
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import javax.swing.JFrame;
 
+
+public class Frm_DanhDachHopDong extends JFrame implements ActionListener, MouseListener{
+
+
+	@Override
+	public void mousePressed(MouseEvent e) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void mouseReleased(MouseEvent e) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void mouseEntered(MouseEvent e) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void mouseExited(MouseEvent e) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void mouseClicked(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
 }
